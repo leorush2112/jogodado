cb2775b8b49c7da6296cddb923705dd3cf173b0e
leomvieira2112 <leonardo.machado1@gmail.com>
2021-07-15 21:37:37 -0300

    sorteio dado mostra resultado

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Dado dado = new Dado(15);
+
+        System.out.println(dado.sortear());
+    }
+}
---
+++ src/Dado.java
@@ -0,0 +1,16 @@
+public class Dado {
+    // Atributos
+    private int quantidadeLados;
+
+    // Construtor
+    public Dado(int quantidadeLados) {
+        this.quantidadeLados = quantidadeLados;
+    }
+
+    // Métodos
+    public int sortear() {
+        double sorteio = Math.random() * quantidadeLados;
+        double numeroArredondado = Math.ceil(sorteio);
+        return (int) numeroArredondado; // casting
+    }
+}
