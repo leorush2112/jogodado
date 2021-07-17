d33b849f68a6695f18d07281500411151d25be07
leomvieira2112 <leonardo.machado1@gmail.com>
2021-07-17 14:09:08 -0300

    Sorteio de 3 numeros e somar

--- src/App.java
+++ src/App.java
@@ -1,8 +1,22 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Dado dado = new Dado(15);
+        Dado dado = new Dado(6);
 
-        System.out.println(dado.sortear());
+        int[] numeros = new int[3];
+
+        int soma = 0;
+
+        for (int i = 0; i < 3; i++) {
+            numeros[i] = dado.sortear();
+        }
+
+        for (int numero : numeros) {
+            soma += numero;
+        }
+
+        System.out.println("Os numeros sorteados são: " + numeros[0] + ", " + numeros[1] + ", " + numeros[2]);
+        System.out.println("A soma é: " + soma);
     }
+
 }
